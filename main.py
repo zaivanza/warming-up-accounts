5f05349d2b72cefd64bc5cdba698f7de8df65e8b
hodlmod.eth <93076644+zaivanza@users.noreply.github.com>
2022-11-07 09:28:47 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,6 @@ import random
 from termcolor import cprint
 import time
 import json
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from termcolor import cprint
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys 
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support.ui import WebDriverWait
-import pyperclip
-import pyotp
 
 RPC = {
         # '1': '',
